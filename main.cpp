7655ce87440da2d52fea2a6fc2666b59a6e26e5d
Anton <anton.ovchinnikov13@gmail.com>
2023-11-19 13:46:37 +0100

    yuri zrobyv ta pokazav

--- main.cpp
+++ main.cpp
@@ -1,69 +1,119 @@
+
 #include <iostream>
-using namespace std;
-/*створити матрицю m x n, якщо матриця квадратна порахувату сумму елементів
- верхнього трикутнка матриці */
+#include <vector>
+#include <cstdlib> //для rand
 
+class ArrayInt3D {
+protected:
+    int nr, nc, nd;  // рядки, стовпці, глибина
+    std::vector<std::vector<std::vector<int>>> a;  // тривимірний масив
 
-//create
-int** createMatrix(int n, int m) {
-    int** matrix = new int*[n]; //масив вказівників
-    for (int i = 0; i < n; i++) {
-        matrix[i] = new int[m];
+public:
+    ArrayInt3D(int N_Rows, int N_Columns, int N_Depth) : nr(N_Rows), nc(N_Columns), nd(N_Depth) {
+        a.resize(nr, std::vector<std::vector<int>>(nc, std::vector<int>(nd)));
+        std::cout << nr << "x" << nc << "x" << nd << std::endl;
+
+        for (int i = 0; i < nr; ++i)
+            for (int j = 0; j < nc; ++j)
+                for (int k = 0; k < nd; ++k) {
+                    std::cout << "a[" << i << "][" << j << "][" << k << "]: ";
+                    std::cin >> a[i][j][k];
+                }
     }
 
-    cout << "Введіть елементи матриці:" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> matrix[i][j];
+    ArrayInt3D(int NR, int NC, int ND, int max) : nr(NR), nc(NC), nd(ND) {
+        a.resize(nr, std::vector<std::vector<int>>(nc, std::vector<int>(nd)));
+        for (int i = 0; i < nr; ++i)
+            for (int j = 0; j < nc; ++j)
+                for (int k = 0; k < nd; ++k)
+                    a[i][j][k] = rand() % (max + 1);
+    }
+
+    ArrayInt3D(int NR, int NC, int ND, int min, int max) : nr(NR), nc(NC), nd(ND) {
+        a.resize(nr, std::vector<std::vector<int>>(nc, std::vector<int>(nd)));
+        for (int i = 0; i < nr; ++i)
+            for (int j = 0; j < nc; ++j)
+                for (int k = 0; k < nd; ++k)
+                    a[i][j][k] = min + rand() % (max - min + 1);
+    }
+
+    void ArrayInt_Out(const std::string& info) {
+        std::cout << info << std::endl;
+        for (int i = 0; i < nr; ++i) {
+            for (int j = 0; j < nc; ++j) {
+                for (int k = 0; k < nd; ++k)
+                    std::cout << a[i][j][k] << "\t";
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
         }
     }
 
-    return matrix; //повернули вказівник
-}
-
-//print
-void printMatrix(int** matrix, int n, int m) {
-    cout << "Ваша матриця:" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
+    int Min_Array(int& kr, int& kc, int& kd) {
+        int min = 2147483647; // маскимальне інтовське значення
+        kr = kc = kd = 0;
+        for (int i = 0; i < nr; ++i)
+            for (int j = 0; j < nc; ++j)
+                for (int k = 0; k < nd; ++k)
+                    if (a[i][j][k] < min) {
+                        min = a[i][j][k];
+                        kr = i;
+                        kc = j;
+                        kd = k;
+                    }
+        return min;
     }
-}
 
-int sum_Matrix(int** matrix, int N) {
-    int sum = 0;
-    for (int i = 0; i < N / 2; i++) {
-        for (int j = i + 1; j < N - i - 1; j++) {
-            sum += matrix[i][j];
-        }
+    int Max_Array(int& kr, int& kc, int& kd) {
+        int max = -2147483648; // мінимальне інтовське занчення
+        kr = kc = kd = 0;
+        for (int i = 0; i < nr; ++i)
+            for (int j = 0; j < nc; ++j)
+                for (int k = 0; k < nd; ++k)
+                    if (a[i][j][k] > max) {
+                        max = a[i][j][k];
+                        kr = i;
+                        kc = j;
+                        kd = k;
+                    }
+        return max;
     }
-    return sum;
-}
+
+    void Set_Element(int kr, int kc, int kd, int elem) {
+        if (kr >= 0 && kr < nr && kc >= 0 && kc < nc && kd >= 0 && kd < nd)
+            a[kr][kc][kd] = elem;
+    }
+
+    int Get_Element(int kr, int kc, int kd) {
+        if (kr >= 0 && kr < nr && kc >= 0 && kc < nc && kd >= 0 && kd < nd)
+            return a[kr][kc][kd];
+    }
+};
 
 int main() {
-    int n, m;
-    cout << "Введіть кількість рядків (n): ";
-    cin >> n;
-    cout << "Введіть кількість стовпців (m): ";
-    cin >> m;
+    // тест рандомного масиву
+    ArrayInt3D arr = ArrayInt3D(2,3,4,1,10);
+    arr.ArrayInt_Out("test");
 
-    // Створюємо матрицю
-    int** matrix = createMatrix(n, m);
+    // min ta max
+    int minRow, minCol, minDepth;
+    int maxRow, maxCol, maxDepth;
 
-    // Виводимо матрицю
-    printMatrix(matrix, n, m);
-    if (n == m) {
-        cout << "Сума  елементів верхнього трикутника: " << sum_Matrix(matrix, n) << endl;
-    } else {
-        cout << "Матриця не квадратна" << endl;
-    }
-    // Звільняємо пам'ять
-    for (int i = 0; i < n; i++) {
-        delete[] matrix[i];
-    }
-    delete[] matrix;
+    int minValue = arr.Min_Array(minRow, minCol, minDepth);
+    std::cout << "Мінімальне значення: " << minValue << " на позиції [" << minRow << "][" << minCol << "][" << minDepth << "]" << std::endl;
+
+
+    int maxValue = arr.Max_Array(maxRow, maxCol, maxDepth);
+    std::cout << "Максимальне значення: " << maxValue << " на позиції [" << maxRow << "][" << maxCol << "][" << maxDepth << "]" << std::endl;
+
+    //тест гетеров та сетеров
+    arr.Set_Element(1, 1, 1, 100); // Встановлення
+    int getElementValue = arr.Get_Element(1, 1, 1);
+    std::cout << "Значення встановленого елементу: " << getElementValue << std::endl;
+
+    arr.ArrayInt_Out("test 2");
+
+
 
     return 0;
 }
